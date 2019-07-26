d8793f34578bfc04832413efd9411c4025abcc51
iamish08 <ashish080397@gmail.com>
2019-07-26 12:45:28 +0530

    assignment 3 fix 2

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -20,13 +20,13 @@ const minLength = (len) => (val) => val && (val.length >= len);
 
  
     
-class CommentForm extends components {
+class CommentForm extends Component {
 
-  consturctor(props){
+  constructor(props){
     super(props);
 
     this.state={
-      isModalOpen = false
+      isModalOpen : false
     };
     this.toggleModal = this.toggleModal.bind(this);
     this.submitHandel = this.submitHandel.bind(this);
@@ -34,22 +34,22 @@ class CommentForm extends components {
 
   toggleModal = () => {
     this.setState( {
-      isModalOpen = !this.state.isModalOpen
+      isModalOpen : !this.state.isModalOpen
     })
   }
   submitHandel(values){
-    console.log("curent state is :" +JSON.stringify(values));
+       console.log("curent state is :" +JSON.stringify(values));
        alert("curent state is :" +JSON.stringify(values));
   }
   render()
   {
     return(
       <React.Fragment>
-        <Button onClick={this.toggleModal}><span className="fa fa-pencil fa-lg">Submit Comment</span></Button>
+        <Button className="bg-white text-dark" onClick={this.toggleModal}><span className="fa fa-pencil fa-lg">Submit Comment</span></Button>
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
           <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
           <ModalBody>
-            <LocalForm onClick={(values) => {this.submitHandel(values)}}>
+            <LocalForm onSubmit={(values) => this.submitHandel(values)}>
           <Row className="form-group">
                             <Label htmlFor="rating" md={4}>Rating</Label>
                             <Col md={12}>
@@ -193,98 +193,3 @@ class CommentForm extends components {
 export default DishDetail;
 
 
-/*
-class CommentForm extends Component {
-  constructor(props)
-  {
-      super(props);
-      this.state={
-          modalOpen:false
-      };
-      this.submitHandle = this.submitHandle.bind(this);
-      this.handleToggle= this.handleToggle.bind(this);
-  }
-
-  handleToggle=()=>
-  {
-      this.setState(
-          {
-              modalOpen:!this.state.modalOpen
-          }
-      )
-  }
-
-  submitHandle(values){
-       console.log("curent state is :" +JSON.stringify(values));
-       alert("curent state is :" +JSON.stringify(values));
-   }
-  render() { 
-      return ( 
-          <React.Fragment>
-          <Button className="bg-white text-dark" onClick={this.handleToggle}><span className="fa fa-pencil fa-lg"></span>{' '}Submit Comment</Button>
-          <Modal isOpen={this.state.modalOpen} toggle={this.handleToggle}>
-              <ModalHeader toggle={this.handleToggle}>
-                  Submit Comment
-              </ModalHeader>
-              <ModalBody>
-                  <LocalForm onSubmit={(value)=>this.submitHandle(value)}>
-                  <Row className="form-group">
-                              <Label htmlFor="rating" md={4}>Rating</Label>
-                              <Col md={12}>
-                                  <Control.select model=".rating" id="rating" name="rating"
-                                      className="form-control"
-                                       >
-                                       <option>1</option>
-                                       <option>2</option>
-                                       <option>3</option>
-                                       <option>4</option>
-                                       <option>5</option>
-                                  </Control.select>
-                              </Col>
-                  </Row>
-                  <Row className="form-group">
-                              <Label htmlFor="author" md={4}>Your Name</Label>
-                              <Col md={12}>
-                                  <Control.text model=".author" id="author" name="author"
-                                      placeholder="Your Name"
-                                      className="form-control"
-                                      validators={{
-                                          minLength: minLength(3), maxLength: maxLength(15), required
-                                      }}
-                                       />
-                                  <Errors
-                                      className="text-danger"
-                                      model=".author"
-                                      show="touched"
-                                      messages={{
-                                        required : 'Reqiuired',
-                                          minLength: 'Must be greater than 2 characters',
-                                          maxLength: 'Must be 15 characters or less'
-                                      }}
-                                   />
-                              </Col>
-                          </Row>
-                          <Row className="form-group">
-                              <Label htmlFor="comment" md={4}>Comment</Label>
-                              <Col md={12}>
-                                  <Control.textarea model=".comment" id="comment" name="comment"
-                                      className="form-control"
-                                      
-                                      rows="6"
-                                       />
-                                 
-                              </Col>
-                          </Row>
-                          <Row className="form-group">
-                          <Col >
-                              <Button type="submit" color="primary" >Submit</Button>
-                          </Col>
-                          </Row>
-                  </LocalForm>
-              </ModalBody>
-          </Modal>
-          </React.Fragment>
-       );
-  }
-}
-*/
