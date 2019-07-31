de558d648a565138aafdddb177ed60887c188300
iamish08 <ashish080397@gmail.com>
2019-07-31 14:13:05 +0530

    redux thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { dishes} from './dishes.js';
 import {Comments} from './comments';
 import {Leaders} from './leaders';
 import {promotions} from './promotions';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -11,7 +14,8 @@ export const ConfigureStore = () => {
             comments : Comments,
             promotions : promotions,
             leaders : Leaders
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );
 
     return store;
